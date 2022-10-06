3a1acd71dd9e54cb75aa7deb824ed3528ecf1792
qwertycho <tychovopstal@gmail.com>
2022-10-06 21:27:17 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tycho van Opstal</title>
+    <link href="./css/style.css" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <h1>Tycho van Opstal</h1>
+        <h2>Software Developer</h2>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="projecten.php">Projecten</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+    <main>
+        <div class="container">
+            <div class="technieken">
+                <h3>Back-end Developer</h3>
+                <div class="techniekIconen">
+                    <div class="techniek">
+                        <p>Javascript</p>
+                    </div>
+                    <div class="techniek">
+                        <p>PHP</p>
+                    </div>
+                    <div class="techniek">
+                        <p>C#</p>
+                    </div>
+                    <div class="techniek">
+                        <p>Linux</p>
+                    </div>
+                </div>
+            </div>
+            <div class="about">
+                <h3>Over Mij</h3>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum quo necessitatibus placeat amet in ipsam perferendis pariatur dolorum soluta asperiores, aliquam laudantium minima doloribus veniam incidunt commodi eos consectetur ratione!
+                </p>
+            </div>
+        </div>
+    </main>
+</body>
+</html>

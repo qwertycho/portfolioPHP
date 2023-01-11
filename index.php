2f5cd274f363a52095fa6e8454bccb3884c778ba
qwertycho <tychovopstal@gmail.com>
2023-01-11 23:32:13 +0100

    bootstrap

--- index.php
+++ index.php
@@ -1,59 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tycho van Opstal</title>
     <link href="./css/style.css" rel="stylesheet">
+    <!-- bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
 </head>
+
 <body>
-    <header>
-        <h1>Tycho van Opstal</h1>
-        <h2>Software Developer</h2>
+    
+    <header class="bg-dark">
+        <h1 class="text-white text-center">Tycho van Opstal</h1>
+        <h2 class="text-white text-center">Software Developer</h2>
+        <?php include './components/nav.html'; ?>
     </header>
-    <?php include './components/nav.html'; ?>
+
+
     <main>
+
+        <h3 class="text-center">
+            Over Mij
+        </h3>
+
         <div class="container">
-            <div class="technieken">
-                <h3>Back-end Developer</h3>
-                <div class="techniekIconen">
-                    <div class="techniek">
-                        <a href="projecten.php?techniek=nodejs">
-                            <img src="./img/node.png" width="90%" alt="node.js">
-                        </a>
-                    </div>
-                    <div class="techniek">
-                        <a href="projecten.php?techniek=php">
-                            <img src="./img/php.png" width="90%" alt="php">
-                        </a>
-                    </div>
-                    <div class="techniek">
-                        <a href="projecten.php?techniek=csharp">
-                            <img src="./img/cSharp.png" width="90%" alt="c#">
-                        </a>
-                    </div>
-                    <div class="techniek">
-                        <a href="projecten.php?techniek=linux">
-                            <img src="./img/tux.png" width="90%" alt="Linux">
-                        </a>
+            <div class="row">
+
+                <div class="col-sm center">
+                    <img class="img-fluid w-75" src="./img/tycho.png" alt="Tycho">
+                </div>
+                <div class="col-sm">
+                    <div class="about">
+                        <p class="text-justify">
+                            Ik ben Tycho van Opstal, een student software developer aan het Grafisch Lyceum Rotterdam! xxxxxxxxxx
+                            Mijn passie voor technologie zorgt ervoor dat geen uitdaging te groot is.
+                            In mijn vrije tijd ben ik (meestal) bezig met allerlei projecten, zoals het ontwikkelen van websites en applicaties.
+                            Maar ook het beheren van mijn eigen homelab waar ik linux servers draai, vooral voor het testen van mijn eigen projecten.
+                            <br>
+                            <br>
+                            Als ik niet bezig ben met projecten houd ik mij bezig met (vr) games, 3d-printen of een van mijn vele andere hobbies.
+                        </p>
                     </div>
                 </div>
             </div>
-            <div class="about">
-                <h3>Over Mij</h3>
-                <p>
-                    Ik ben Tycho van Opstal, een student software developer aan het Grafisch Lyceum Rotterdam! xxxxxxxxxx
-                    Mijn passie voor technologie zorgt ervoor dat geen uitdaging te groot is.
-                    In mijn vrije tijd ben ik (meestal) bezig met allerlei projecten, zoals het ontwikkelen van websites en applicaties.
-                    Maar ook het beheren van mijn eigen homelab waar ik linux servers draai, vooral voor het testen van mijn eigen projecten.
-                    <br>
-                    <br>
-                    Als ik niet bezig ben met projecten houd ik mij bezig met (vr) games, 3d-printen of een van mijn vele andere hobbies.
-                </p>
-                <img class="tycho" src="./img/tycho.png" width="90%" alt="Tycho">
-            </div>
         </div>
+
     </main>
+
+
 </body>
+
 </html>
